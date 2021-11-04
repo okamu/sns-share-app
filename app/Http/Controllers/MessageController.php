4d680bf021ffe83d82fd813ad3be55e9dca8bb97
okamu <okamuka28@gmail.com>
2021-11-05 02:57:45 +0900

    add message registration process to store function at MessageController

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,11 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
+use DB;
+use Exeption;
 use Illuminate\Http\Request;
+use Str;
 
-class MessageController extends Controllers{
+class MessageController extends Controller{
 
     public function store(Request $request){
+        $params = $request->json()->all();
+        $content = $params['message'];
+        $id = Str::uuid();
 
+        $file = $id->toString() . '.jpg';
+
+        Message::create([
+            'id' => $id,
+            'content' => $content,
+            'file_path' => $file,
+        ]);
+
+        return response()->json($id);
     }
 }
