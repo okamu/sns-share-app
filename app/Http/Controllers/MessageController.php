1c35330e95276be3ecc14d6a6ce015d793a7527b
okamu <okamuka28@gmail.com>
2021-11-06 02:29:16 +0900

    image upload process

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,22 +6,42 @@ use App\Models\Message;
 use DB;
 use Exeption;
 use Illuminate\Http\Request;
+use Storage;
 use Str;
 
 class MessageController extends Controller{
 
     public function store(Request $request){
         $params = $request->json()->all();
+
+        list(, $image) = explode(';', $params['image']);
+        list(, $image) = explode(',', $image);
+        $decodedImage = base64_decode($image);
+
         $content = $params['message'];
+        
+        $id = DB::transaction(function() use ($decodedImage, $content){
         $id = Str::uuid();
 
         $file = $id->toString() . '.jpg';
 
-        Message::create([
-            'id' => $id,
-            'content' => $content,
-            'file_path' => $file,
-        ]);
+            Message::create([
+                'id' => $id,
+                'content' => $content,
+                'file_path' => $file,
+            ]);
+
+        $isSuccess = Storage::disk('s3')->put($file, $decodedImage);
+
+        if(!$isSuccess){
+            throw new Exception('ファイルアップロード時にエラーが発生しました。');
+        }
+
+        Storage::disk('s3')->setVisibility($file, 'public');
+
+        return $id;
+
+        });
 
         return response()->json($id);
     }
